e6eeb1af61dbdb40366abc6daacd31aa4f906f0c
Nor <86627882+vNxr@users.noreply.github.com>
2021-06-29 22:11:25 -0400

    Create Rock-Paper-Scissors.py

---
+++ Rock-Paper-Scissors.py
@@ -0,0 +1,24 @@
+import random
+
+player_action = input("Coded by vNxr aka Nor, Enter a choice (rock, paper, scissors): ")
+maybe_actions = ["rock", "paper", "scissors"]
+AI_action = random.choice(maybe_actions)
+print(f"\nYou chose {player_action}, AI chose {AI_action}.\n")
+
+if player_action == computer_action:
+    print(f"Each player chose {player_action}. It's specified as a tie!")
+elif player_action == "rock":
+    if AI_action == "scissors":
+        print("Rock slams scissors! You gained a win!")
+    else:
+        print("Paper covers rock! You lost.")
+elif player_action == "paper":
+    if AI_action == "rock":
+        print("Paper covers rock! You gained a win!")
+    else:
+        print("Scissors cuts paper! You lost.")
+elif player_action == "scissors":
+    if AI_action == "paper":
+        print("Scissors slices paper! You gained a win!")
+    else:
+        print("Rock slams scissors! You lost.")
